bd8945f2e9dfbfc94d3e0cb12a5717b21ad94721
Mateus Fonseca <mateusrfon@gmail.com>
2021-04-13 19:07:36 -0300

    post error/success

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,9 @@
-const promessa = axios.get('https://mock-api.bootcamp.respondeai.com.br/api/v2/moviefinder/filmes');
-promessa.then(ReceiveMovies)
+MoviesAquisition();
+
+function MoviesAquisition() {
+    const promessa = axios.get('https://mock-api.bootcamp.respondeai.com.br/api/v2/moviefinder/filmes');
+    promessa.then(ReceiveMovies)
+}
 
 function ReceiveMovies(movies) {
     const moviesList = movies.data;
@@ -11,11 +15,33 @@ function ReceiveMovies(movies) {
         <div class="movie">
             <img src="${moviesList[i].imagem}">
             <div class="title">${moviesList[i].titulo}</div>
-            <button>
+            <button onclick="Buy(${moviesList[i].id})">
                 Comprar
                 <ion-icon name="cart-outline"></ion-icon>
             </button>
         </div>
         `;
     }
+}
+
+function Buy(id) {
+    const purchase = {};
+    purchase.nome = prompt('Qual o seu nome?')
+    purchase.quantidade = prompt('Quantos assentos deseja?')
+
+    const promessa = axios.post(
+        `https://mock-api.bootcamp.respondeai.com.br/api/v2/moviefinder/filmes/${id}/ingresso`,
+        purchase
+    );
+
+    promessa.then(PurchaseSuccess)
+    promessa.catch(PurchaseError)
+}
+
+function PurchaseSuccess(){
+    alert("Ingresso comprado com sucesso!");
+}
+
+function PurchaseError() {
+    alert("Os ingressos para este filme estão esgotados!");
 }
